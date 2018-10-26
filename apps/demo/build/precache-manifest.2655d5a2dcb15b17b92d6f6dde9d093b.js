e2e6804e07cc4521b2d79fcc8514b1f0cbdd9e9b
Joe Sullivan <josulliv101@gmail.com>
2018-10-26 03:43:35 -0400

    build

---
+++ apps/demo/build/precache-manifest.2655d5a2dcb15b17b92d6f6dde9d093b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d7b1a1824f65a6d9e33e",
+    "url": "/static/js/main.d7b1a182.chunk.js"
+  },
+  {
+    "revision": "3325df5b139ffd33237e",
+    "url": "/static/js/1.3325df5b.chunk.js"
+  },
+  {
+    "revision": "d7b1a1824f65a6d9e33e",
+    "url": "/static/css/main.83ed39cc.chunk.css"
+  },
+  {
+    "revision": "8e8cd8fe05ac87f0ef371ec2750f5c48",
+    "url": "/index.html"
+  }
+];
